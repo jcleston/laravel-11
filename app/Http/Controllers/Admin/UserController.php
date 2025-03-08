cc091f694b0b96ee80fada7c6b298816443faf4c
Janes Cleston <janes.cleston.silva@gmail.com>
2025-03-08 17:33:46 -0300

    refactor: update UserController add paginate

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -14,86 +14,9 @@ class UserController extends Controller
 {
     public function index()
     {
-        //$users = User::paginate(15); // User::all();
-
-        //return view('admin.users.index', compact('users'));
-        $user = User::first();
-
-        if ($user == null) {
-            return 'sem usuário';
-        } else {
-            //return $user->name;
-            return view('admin.users.index', compact('user'));
-        }
-
-        //return $user->name;
+        $users = User::paginate(15);//User::all();
+        //dd($users);
+        return view('admin.users.index', compact('users'));
+  
     }
-
-    /*    public function create()
-    {
-        return view('admin.users.create');
-    }
-
-    public function store(StoreUserRequest $request)
-    {
-        User::create($request->validated());
-
-        return redirect()
-            ->route('users.index')
-            ->with('success', 'Usuário criado com sucesso');
-    }
-
-    public function edit(string $id)
-    {
-        // $user = User::where('id', '=', $id)->first();
-        // $user = User::where('id', $id)->first(); // ->firstOrFail();
-        if (!$user = User::find($id)) {
-            return redirect()->route('users.index')->with('message', 'Usuário não encontrado');
-        }
-
-        return view('admin.users.edit', compact('user'));
-    }
-
-    public function update(UpdateUserRequest $request, string $id)
-    {
-        if (!$user = User::find($id)) {
-            return back()->with('message', 'Usuário não encontrado');
-        }
-        $data = $request->only('name', 'email');
-        if ($request->password) {
-            $data['password'] = bcrypt($request->password);
-        }
-        $user->update($data);
-
-        return redirect()
-            ->route('users.index')
-            ->with('success', 'Usuário editado com sucesso');
-    }
-
-    public function show(string $id)
-    {
-        if (!$user = User::find($id)) {
-            return redirect()->route('users.index')->with('message', 'Usuário não encontrado');
-        }
-
-        return view('admin.users.show', compact('user'));
-    }
-
-    public function destroy(string $id)
-    {
-        // if (Gate::denies('is-admin')) {
-        //     return back()->with('message', 'Você não é um administrador');
-        // }
-        if (!$user = User::find($id)) {
-            return redirect()->route('users.index')->with('message', 'Usuário não encontrado');
-        }
-        if (Auth::user()->id === $user->id) {
-            return back()->with('message', 'Você não pode deletar o seu próprio perfil');
-        }
-        $user->delete();
-
-        return redirect()
-            ->route('users.index')
-            ->with('success', 'Usuário deletado com sucesso');
-    }*/
 }
