2c5c19b5f912e7a578bf8f084a48fa73c5b662a1
Janes Cleston <janes.cleston.silva@gmail.com>
2025-03-09 17:56:59 -0300

    refactor: updated form views

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -4,14 +4,7 @@
 
 <h1>Novo usuário</h1>
 
-<x-alert />
-
 <form action="{{ route('users.store') }}" method="post">
-    @csrf()
-    <input type="text" name="name" placeholder="Nome" value="{{ old('name') }}">
-    <input type="text" name="email" placeholder="Email" value="{{ old('email') }}">
-    <input type="password" name="password" placeholder="Senha">
-
-    <button type="submit">Cadastrar</button>
+    @include('admin.users.partials.form')
 </form>
 @endsection
--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -5,21 +5,8 @@
 <h1>Editar usuário {{ $user->name }}</h1>
 <x-alert />
 
-@if ($errors->any())
-<ul>
-    @foreach ($errors->all() as $error)
-    <li>{{ $error }}</li>
-    @endforeach
-</ul>
-@endif
-
 <form action="{{ route('users.update', $user->id) }}" method="post">
-    @csrf()
     @method('PUT')
-    <input type="text" name="name" placeholder="Nome" value="{{ $user->name }}">
-    <input type="text" name="email" placeholder="Email" value="{{ $user->email }}">
-    <input type="password" name="password" placeholder="Senha">
-
-    <button type="submit">Editar</button>
+    @include('admin.users.partials.form')
 </form>
 @endsection
